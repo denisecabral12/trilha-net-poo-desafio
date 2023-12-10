ed2685ed6267bfe8c5c9c27fbf1d662a97a41179
Denise Cabral Lopes <denise.cabral12@gmail.com>
2023-12-10 09:41:33 -0300

    criação de classes com modelos de smartphone and implementação de teste

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Iphone iphone = new Iphone("987654321", "Iphone 11", "987654321", 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
